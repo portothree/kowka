f44ac805b9a82986369428d0ff34797fd91f5ba8
porto <portothree@gmail.com>
2019-10-02 12:39:11 +0100

    :recycle: changing how to create and manage tasks to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 const gulp = require("gulp");
 const fileinclude = require("gulp-file-include");
-const browserSync = require("browser-sync").create();
-const sass = require("gulp-sass");
 
 const app = {
   srcPath: "src/",
   prdPath: "dist/"
 };
 
-gulp.task("html", async () => {
+async function html() {
   gulp
     .src(app.srcPath + "pages/*.html")
     .pipe(
@@ -18,28 +16,6 @@ gulp.task("html", async () => {
       })
     )
     .pipe(gulp.dest(app.prdPath))
-    .pipe(browserSync.stream());
-});
+};
 
-gulp.task("sass", () => {
-  return gulp
-    .src("src/common.blocks/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest(app.prdPath + "/css"))
-    .pipe(browserSync.stream());
-});
-
-// make sure that the tasks are completed before reloading
-gulp.task("html-watch", ["html"], browserSync.reload);
-gulp.task("sass-include", ["sass"], browserSync.reload);
-
-gulp.task("default", ["html", "sass"], () => {
-  browserSync.init({
-    server: {
-      baseDir: app.prdPath
-    }
-  });
-
-  gulp.watch("src/pages/*.html", ["html-watch"]);
-  gulp.watch("src/common.blocks/*.scss", ["sass-include"]);
-});
+exports.html = html;
