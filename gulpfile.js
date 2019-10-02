c4d2b283615f7e42298bf5b395b5a8c77fade35c
porto <portothree@gmail.com>
2019-10-02 12:20:40 +0100

    :wrench: config browser sync and update gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const fileinclude = require("gulp-file-include");
 const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
 
 const app = {
   srcPath: "src/",
@@ -16,13 +17,29 @@ gulp.task("html", async () => {
         basepath: "@file"
       })
     )
-    .pipe(gulp.dest(app.prdPath));
+    .pipe(gulp.dest(app.prdPath))
+    .pipe(browserSync.stream());
 });
 
-gulp.task("browser-sync", () => {
+gulp.task("sass", () => {
+  return gulp
+    .src("src/common.blocks/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest(app.prdPath + "/css"))
+    .pipe(browserSync.stream());
+});
+
+// make sure that the tasks are completed before reloading
+gulp.task("html-watch", ["html"], browserSync.reload);
+gulp.task("sass-include", ["sass"], browserSync.reload);
+
+gulp.task("default", ["html", "sass"], () => {
   browserSync.init({
     server: {
       baseDir: app.prdPath
     }
   });
+
+  gulp.watch("src/pages/*.html", ["html-watch"]);
+  gulp.watch("src/common.blocks/*.scss", ["sass-include"]);
 });
