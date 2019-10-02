a82dbd30dc139d9aa60595dc940354626e73eecf
porto <portothree@gmail.com>
2019-10-02 15:45:40 +0100

    :sparkles: concatenate all scss files into one unique css

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,50 @@
-const gulp = require("gulp");
+const { src, dest, watch, series, parallel } = require("gulp");
 const fileinclude = require("gulp-file-include");
+const sass = require("gulp-sass");
+const concat = require("gulp-concat");
+const browserSync = require("browser-sync").create();
 
 const app = {
   srcPath: "src/",
-  prdPath: "dist/"
+  prdPath: "dist/",
+
+  scssPath: "src/common.blocks/**/*.scss",
+  htmlPath: "src/pages/*.html"
 };
 
-async function html() {
-  gulp
-    .src(app.srcPath + "pages/*.html")
+function html(done) {
+  src(app.htmlPath)
     .pipe(
       fileinclude({
         prefix: "@@",
         basepath: "@file"
       })
     )
-    .pipe(gulp.dest(app.prdPath))
-};
+    .pipe(dest(app.prdPath))
+    .pipe(browserSync.stream());
 
-exports.html = html;
+  done();
+}
+
+function style(done) {
+  src(app.scssPath)
+    .pipe(sass())
+    .pipe(concat("style.css"))
+    .pipe(dest(app.prdPath + "css"))
+    .pipe(browserSync.stream());
+
+  done();
+}
+
+function watchTask() {
+  browserSync.init({
+    server: {
+      baseDir: app.prdPath
+    }
+  });
+
+  watch(app.htmlPath).on("change", html);
+  watch(app.scssPath, style);
+}
+
+exports.default = series(parallel(html, style), watchTask);
